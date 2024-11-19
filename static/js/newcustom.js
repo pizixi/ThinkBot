5531eb68fac1ac00de6c02e60d83de96478b1371
pizixi <pizixi@proton.me>
2024-11-19 14:42:06 +0800

    查询可用不用刷新页面，增加设置页面的滚动条

--- static/js/newcustom.js
+++ static/js/newcustom.js
@@ -246,4 +246,22 @@ $(document).ready(function () {
 	var defaultProvider = $('#serviceProvider').val();
 	updateProviderSettings(defaultProvider); // 加载服务商设置
 	restoreSelectedModelInfo(); // 恢复选中的模型信息
+
+	// 添加新的事件监听器
+	$(document).ready(function() {
+		// 当点击模型选择框时触发
+		$('#model').on('select2:open', function() {
+			var modelEndpoint = $('#modelEndpoint').val();
+			var oneHubToken = $('#oneHubToken').val();
+			
+			if (modelEndpoint && oneHubToken) {
+				updateModelSelect([]);  // 传入空数组作为selectedModels
+			}
+		});
+
+		// 当modelEndpoint或oneHubToken值改变时也更新模型列表
+		$('#modelEndpoint, #oneHubToken').on('change', function() {
+			updateModelSelect([]);
+		});
+	});
 });
